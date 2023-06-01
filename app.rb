533e6487a5099b8dc7b955f3d04261f487451eec
krillan <pushkinidze49@gmail.com>
2023-06-01 12:59:12 +0300

    некоторые методы добавления характеристик экспы и повышения левела

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ def character_panel(char)
   puts '--------------------------------------------------------------------------------------------'
   puts '--------------------------------------------------------------------------------------------'
   puts char.name
-  puts "Уровень #{char.lvl} (#{char.exp}/#{char.exp_lvl[char.lvl + 1]})"
+  puts "Уровень #{char.lvl} (#{char.exp}/#{char.exp_lvl[char.lvl + 1].min})"
   puts "Н А В Ы К И:"
   puts "[акт] #{@name_special_pl} #{@lor_special_pl}"
   puts "[пас] #{@name_passive_pl} #{@lor_passive_pl}"
@@ -46,14 +46,48 @@ class Character
     @points = 5
     @skil_points = 0
 
-    @exp_lvl = [0, 2, 5, 9, 14, 20, 27, 35, 44, 54, 65, 77, 90, 104, 129, 145, 162, 180, 200]
+    @exp_lvl = [0..1, 2..4, 5..8, 9..13, 14..19, 20..26, 27..34, 35..43, 44..53, 54..64, 65..76, 77..89, 90..103, 104..999]
+  end
+
+  def full_points
+    @hp = @max_hp
+    @mp = @max_mp
   end
 
   def name_change(name)
     @name = name
   end
+
+  def plus_hp(n)
+    @max_hp += n
+  end
+
+  def plus_mp(n)
+    @max_mp += n
+  end
+
+  def plus_damage(n)
+    @mindam == @maxdam ? @maxdam += n : [@mindam, @maxdam][rand(1)] += n
+  end
+
+  def plus_exp_and_level_up(n)
+    @exp += n
+    new_lvl = @exp_lvl.index{|e| e.include?(@exp)} - @lvl # ???
+    @lvl += new_lvl
+    @points += new_lvl
+    @skil_points += new_lvl
+  end
+
+  def points_converter(n)
+
+  end
 end
 
 char = Character.new
 char.name_change('Vasyan')
-character_panel(char)
+
+loop do
+  character_panel(char)
+  char.plus_exp_and_level_up(4)
+  gets
+end
